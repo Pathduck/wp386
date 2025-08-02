32fe329a799c03d8605a974719187fb9dabd6fda
Stian <1493836+Pathduck@users.noreply.github.com>
2025-08-02 12:57:08 +0200

    Update footer

--- footer.php
+++ footer.php
@@ -11,9 +11,9 @@
 				<footer id="colophon" class="site-footer" role="contentinfo">
 					<div class="site-info container">
 						<?php do_action( 'wp386_credits' ); ?>
-						<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wp386' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wp386' ), 'WordPress' ); ?></a>
+						<a href="https://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wp386' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wp386' ), 'WordPress' ); ?></a>
 						<span class="sep"> | </span>
-						<?php printf( __( 'Theme: %1$s by %2$s.', 'wp386' ), 'wp386', '<a href="http://themes.kkob.us/wp386" rel="designer">Keitaroh Kobayashi</a>' ); ?>
+						<?php printf( __( 'Theme: %1$s by %2$s.', 'wp386' ), 'wp386', '<a href="https://github.com/keichan34" rel="designer">Keitaroh Kobayashi</a>' ); ?>
 					</div><!-- .site-info -->
 				</footer><!-- #colophon -->
 
